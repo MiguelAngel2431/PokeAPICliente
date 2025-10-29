60a9b0c1be89c5c2b19c6f75ba24e9afe1617e7d
Alien 5 <Alien 5@192.167.1.42>
2025-10-29 14:21:54 -0600

    Creación de repositorio

---
+++ src/main/java/com/digis01/PokeAPICliente/PokeApiClienteApplication.java
@@ -0,0 +1,13 @@
+package com.digis01.PokeAPICliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiClienteApplication.class, args);
+	}
+
+}
