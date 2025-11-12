2789b2df234a29af3f6b5c03dd8df4d2658a39c4
Alien 5 <Alien 5@192.167.1.42>
2025-11-12 16:35:56 -0600

    Final ahora si

---
+++ src/main/java/com/digis01/PokeAPICliente/Configuration/CrossFilterConfig.java
@@ -0,0 +1,32 @@
+
+package com.digis01.PokeAPICliente.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CrossFilterConfig {
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        
+        corsConfig.addAllowedOrigin("*");
+        
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("DELETE");
+        corsConfig.addAllowedMethod("PATCH");
+        
+        corsConfig.addAllowedHeader("*");
+        
+        source.registerCorsConfiguration("/**", corsConfig);
+        
+        return new CorsFilter(source);
+    }
+}
---
+++ src/main/java/com/digis01/PokeAPICliente/Configuration/WebConfig.java
@@ -0,0 +1,24 @@
+
+package com.digis01.PokeAPICliente.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:8080", "http://192.167.0.73:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
